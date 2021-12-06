c5919f7c1224a9fdd24cc5c998a0ea6d72ad3b80
Enrique-Lopez-Lopez <enrique.lpz.1510@gmail.com>
2021-12-05 23:09:58 -0600

    S30 Middlewares con express

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
 //Importar el modulo http
 import http from 'http';
+//Importando el module de routes
 import {requestHandler} from "./routes.js"
 //3.- Importando express
+// $ npm i express -s
 import Express from 'express'
+import { addAbortSignal } from 'stream';
 
 // Crea una instancia de Express
 const app = Express();  // (req, res, next)=>{} event handler
+
+// Registrando el primer middleware
+app.use((req, res, next)=>{
+    //Registrar un mensaje en el log
+    console.log("Estoy en el middleware 1");
+    // Dar la instruccion de pasar el siguiente middleware
+    next();
+});
+//Registrando el segundo middleware
+app.use((req, res, next)=>{
+    //Registrar un mensaje en el log
+    console.log("Estoy en el middleware 2");
+    // Dar la instruccion de pasar el siguiente middleware
+    next();
+});
+app.use((_, res)=>{
+    //Registrar un mensaje en el log
+    console.log("Estoy en el middleware 3");
+    console.log("Emitiendo respuesta a cliente");
+    res.send("<h1>Mi respuesta</h1> \n Hola");
+});
 
 /**
  * Codigos de Emojies
  * ref: https://www.w3schools.com/Chassets/ref_emoji.asp
  */
 
-// 2.- crear un servidor tomando como 
-//  manejadpr de peticiones a express
-
-const server = http.createServer(app);
-    
-
-// 3.-Pongo a trabajar el servidor
-//Le paso un callback que escribira en la consola
-// Cuando el servidor este escuchando  
-//192.168.100.136
-server.listen(3000, '0.0.0.0', () => {
-    console.log("Servidor escuchandoen http://0.0.0.0:3000");
+//Poniendo a escuchar la app de express
+app.listen(3000, '0.0.0.0',()=>{
+    console.log("Servidor escuchando en http://0.0.0.0:3000");
 });
+
 
