2b448ee0a04f736128a0acf875b6e0e1d1a61831
Enrique-Lopez-Lopez <enrique.lpz.1510@gmail.com>
2021-12-09 09:16:48 -0600

    S31 Procesando un formulario

--- index.js
+++ index.js
@@ -9,16 +9,48 @@ import Express from 'express'
 // Crea una instancia de Express
 const app = Express();  // (req, res, next)=>{} event handler
 
-// La ruta raiz entra en todo tipo de peticion
+//1. Insertando Middleware para la lectura de datos
+// desde un cliente
+app.use(Express.urlencoded({extended:false}));
+
+//Loggin de peticiones
+app.use((req,res,next)=>{
+    console.log(`Se ha realizado la peticion: "${req,method}:${req.path}`);
+    next();
+});
+
+// Se debe colocar primero ya que el orden de registro 
+// determina el orden de verificacion
 app.use('/about',(_, res)=>{
-    console.log('Se ha realizado la peticion: "/about"')
     res.send("<h1>Acerca de ...</h1>\n Sitio inicial hecho con Node.js");
 });
-// Se debe colocar ya que el orden de registro 
-// determine el orden de verificacion
-app.use(['/', '/home'](_, res)=>{
+
+//Sirviendo recurso de formulario
+app.use('/add-student-form', (req, res, next)=> {
+    res.send(`<form action="/add-student" method = "POST">
+        <label fro="student-name"> Student Name </label>
+        <input type="text" name="name" id = "student-name">
+        </button type="submit">Add student</button>
+    </form>
+    `);
+});
+
+//Ruta que procesa el formulario
+app.use('/add-student',(req, res, next)=>{
+    //Iterando sobre todo el objeto
+    for(const prop in req.body){
+        console.log(`${prop}:${req[prop]}`);
+    }
+    console.log(`Metodo: ${req.method}`);
+    
+    res.json(req.body);
+    //REalizaos un redireccinamiento 
+    // res.redirect('/');
+}); 
+
+//La ruta raiz entra en todo tipo de peticion
+app.use(['/', '/home'],(_, res)=>{
     //Registrar un mensaje en el log
-    console.log('Se ha realizado la peticion: "/"');
     res.send("<h1>Mi App</h1> \n Bienvenido a este sitio");
 });
 
